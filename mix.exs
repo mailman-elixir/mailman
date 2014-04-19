6c8c1afa0847fce48e0d8fc9bec3bf05bca2f53a
Kamil Ciemniewski <ciemniewski.kamil@gmail.com>
2014-04-19 12:08:08 +0200

    Switch to Elixir 0.13

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mailman.Mixfile do
   def project do
     [ app: :mailman,
       version: "0.0.2",
-      elixir: "~> 0.11",
+      elixir: "~> 0.13.0-dev",
       deps: deps ]
   end
 
@@ -17,7 +17,7 @@ defmodule Mailman.Mixfile do
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [ 
-      { :gen_smtp, "~> 0.1", git: "https://github.com/Vagabond/gen_smtp.git" }
+      { :gen_smtp, ~r/0\.1/, git: "https://github.com/Vagabond/gen_smtp.git" }
     ]
   end
 end
