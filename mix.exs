c64426dcb3d1a85b00cf894bc2630dc38513690b
Kamil Ciemniewski <ciemniewski.kamil@gmail.com>
2015-10-12 16:44:50 +0200

    Bumped the version number

--- mix.exs
+++ mix.exs
@@ -8,8 +8,8 @@ defmodule Mailman.Mixfile do
       homepage_url: "https://github.com/kamilc/mailman",
       description: "Library providing a clean way of defining mailers in Elixir apps",
       package: package,
-      version: "0.2.0",
-      elixir: "~> 1.0.0",
+      version: "0.2.1",
+      elixir: "~> 1.0",
       deps: deps ]
   end
 
@@ -32,7 +32,7 @@ defmodule Mailman.Mixfile do
   defp package do
     [
       files: ["lib", "docs", "LICENSE", "README", "mix.exs"],
-      contributors: ["Kamil Ciemniewski <ciemniewski.kamil@gmail.com>"],
+      maintainers: ["Kamil Ciemniewski <ciemniewski.kamil@gmail.com>"],
       licenses: ["MIT"],
       links: %{
          "GitHub" => "https://github.com/kamilc/mailman",
