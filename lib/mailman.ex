63adfef7a1b216fd24721942f07ebf99943e2ab1
Max Neuvians <max@neuvians.net>
2015-12-30 09:23:11 -0500

    Added deliver/3 to send CC and BCC emails

--- lib/mailman.ex
+++ lib/mailman.ex
@@ -15,4 +15,27 @@ defmodule Mailman do
     message = Mailman.Render.render(email, context.composer)
     Adapter.deliver(context.config, email, message)
   end
+
+  @doc "Delivers given email to all addresses and returns a list of Tasks"
+  def deliver(email, context, :send_cc_and_bcc) do
+    bcc_list = email.bcc
+    cleaned_email = %Mailman.Email{email | bcc: []}
+    message = Mailman.Render.render(cleaned_email, context.composer)
+
+    to_task = [Adapter.deliver(context.config, email, message)]
+
+    cc_tasks = email.cc |> Enum.map(fn(address) ->  
+      cc_envelope = %Mailman.Email{email | to: [address]}
+      Adapter.deliver(context.config, cc_envelope, message)
+    end)
+
+    bcc_tasks = bcc_list |> Enum.map(fn(address) ->  
+      bcc_envelope = %Mailman.Email{email | to: [address]}
+      bcc_message = %Mailman.Email{email | bcc: [address]}
+      message = Mailman.Render.render(bcc_message, context.composer)
+      Adapter.deliver(context.config, bcc_envelope, message)
+    end)
+
+    to_task ++ cc_tasks ++ bcc_tasks
+  end
 end
