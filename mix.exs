f9919d61268286a6d7dd644e5342f251740b0795
Sebastian Kosch <sebastian@aldusleaf.org>
2020-09-26 15:55:30 -0700

    Update gen_smtp to 0.15

--- mix.exs
+++ mix.exs
@@ -34,8 +34,7 @@ defmodule Mailman.Mixfile do
   defp deps do
     [
       {:eiconv, "~> 1.0.0"},
-    # {:gen_smtp, "~> 0.15.0"},
-      {:gen_smtp, github: "gen-smtp/gen_smtp", override: true},
+      {:gen_smtp, "~> 0.15.0"},
       {:ex_doc, ">= 0.19.1", only: :dev},
       {:httpoison, "~> 1.6"},
       {:credo, "~> 0.10", only: [:dev, :test], runtime: false}
